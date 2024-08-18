976423150d251a6614b1ff8ab9b300587c221144
Zouhirborali <zouhirbakkas@gmail.com>
2024-08-18 11:48:46 +0100

    fix var

--- test/change_var_ulit.c
+++ test/change_var_ulit.c
@@ -6,7 +6,7 @@
 /*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 20:23:37 by zbakkas           #+#    #+#             */
-/*   Updated: 2024/08/16 21:29:58 by zbakkas          ###   ########.fr       */
+/*   Updated: 2024/08/18 11:48:24 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static int	get_name_var_count(char *str)
 	{
 		if (is_v)
 		{
-			if (!ft_isalpha(str[x]) && !ft_isdigit(str[x]) && str[x] != '_' && str[x-1]!= '$')
+			if ((!ft_isalpha(str[x]) && !ft_isdigit(str[x]) && str[x] != '_' && str[x-1]!='$')|| (!ft_isalpha(str[x-1]) && str[x-1]!='$'))
 				break ;
 			i++;
 		}
@@ -65,13 +65,14 @@ char	*get_name_var(char *str, int *j)
 	x = 0;
 	is_v = 0;
 	i = 0;
-	printf("count%d\n",get_name_var_count(str));
 	re = malloc(get_name_var_count(str) + 1);
 	while (str[x])
 	{
 		if (is_v)
 		{
-			if (!ft_isalpha(str[x]) && !ft_isdigit(str[x]) && str[x] != '_' && str[x-1]!= '$')
+			if ((!ft_isalpha(str[x]) && !ft_isdigit(str[x]) 
+					&& str[x] != '_' && str[x - 1] != '$') 
+				|| (!ft_isalpha(str[x - 1]) && str[x - 1] != '$'))
 				break ;
 			re[i++] = str[x];
 			(*j)++;
@@ -80,7 +81,5 @@ char	*get_name_var(char *str, int *j)
 			is_v = 1;
 		x++;
 	}
-	re[i] = '\0';
-	printf("name;%s\n",re);
-	return ( (re));
+	return (re[i] = '\0', (re));
 }
